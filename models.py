5785858a9a1f47edfdbe84bddc58ed69fd8a47fa
Matthew van Rijn <matthew.van.rijn@gmail.com>
2018-11-07 13:44:17 +0100

    encoder

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from torch.nn.utils.rnn import pack_sequence
+from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence
 
 class Encoder(nn.Module):
   def __init__(self, embeddings, hidden_size=500):
@@ -14,10 +14,15 @@ class Encoder(nn.Module):
   def forward(self, input):
     embedded_input = [self.emb(x) for x in input]
     packed_sequences, restore_order = self.pack(embedded_input)
-    _, (lstm_hidden, _) = self.lstm(packed_sequences)
+    lstm_output, (lstm_hidden, _) = self.lstm(packed_sequences)
+
+    # For bilinear
     final_hidden = lstm_hidden.view(2, 2, -1, self.hidden_size)[1].view(-1, self.hidden_size*2)
 
-    return final_hidden
+    # For decoder
+    hidden_concat = self.unpack(lstm_output)
+
+    return final_hidden, hidden_concat
 
   @staticmethod
   def pack(input):
@@ -32,8 +37,35 @@ class Encoder(nn.Module):
 
     return packed_sequences, reverse_order
 
+  def unpack(self, packed_sequence):
+    unpacked_sequence, lengths = pad_packed_sequence(packed_sequence)
+
+    hidden_concats = []
+    for i in range(len(lengths)):
+      hidden_concats.append(unpacked_sequence[:lengths[i],i].contiguous().view(lengths[i]*self.hidden_size*2))
+
+    return hidden_concats
+
+class Bilinear(nn.Module):
+  def __init__(self, hidden_size=500):
+    super().__init__()
+
+    self.bifc = nn.Bilinear(2*hidden_size, 2*hidden_size, 1)
+    self.sigmoid = nn.Sigmoid()
+
+  def forward(self, x1, x2):
+    bi_out = self.bifc(x1, x2)
+
+    return self.sigmoid(bi_out)
+
 x = [[4, 5, 2], [2, 1], [1, 1, 1, 1]]
 x = [torch.tensor(i) for i in x]
 emd = nn.Embedding(10, 500)
 e = Encoder(emd)
-e(x)
+b = Bilinear()
+u, j = e(x)
+s = b(u, u)
+
+print(j)
+for t in j:
+  print(t.size())
