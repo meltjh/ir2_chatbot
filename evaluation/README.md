f4acd2ad22158e105c1c48520faf89e130e5d9bc
Richard Olij <richardolij@Richards-MacBook-Pro.local>
2018-11-29 13:24:03 +0100

    Evaluation

---
+++ evaluation/README.md
@@ -0,0 +1,3 @@
+## Evaluation
+Code from https://github.com/nikitacs16/Holl-E/
+in result_data store a txt file for input and one for response where the sentences are seperated by an enter. The sentences from each row of both files are compared together.
---
+++ evaluation/evaluate.py
@@ -0,0 +1,27 @@
+"""
+    Code from https://github.com/nikitacs16/Holl-E/
+"""
+
+import sys
+import glob
+import rouge
+import bleu
+f1 = sys.argv[1] #decoded
+f2 = sys.argv[2] #reference
+ref = []
+decoded = []
+
+for i, j in zip(sorted(glob.glob(f1+'*.txt')),sorted(glob.glob(f2+'*.txt'))):
+        ref_tex = ''
+        dec_tex = ''
+        for k in open(i).readlines():
+                dec_tex = dec_tex + k.strip()
+        for l in open(j).readlines():
+                ref_tex = ref_tex + l.strip()
+        ref.append(ref_tex)
+        decoded.append(dec_tex)
+
+bl = bleu.moses_multi_bleu(decoded,ref)
+x = rouge.rouge(decoded,ref)
+print('%.2f\t%.2f\t%.2f\t%.2f'%(bl,x['rouge_1/f_score']*100,x['rouge_2/f_score']*100,x['rouge_l/f_score']*100))
+
