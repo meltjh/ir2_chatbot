c2c559950c26c5b4b47026fc34bea9f939f50fc4
Richard Olij <richardolij@Richards-MacBook-Pro.local>
2018-12-04 21:11:33 +0100

    Evaluation

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -6,48 +6,66 @@ import glob
 import rouge
 import bleu
 import numpy as np
+import matplotlib.pyplot as plt
 
-f1 = sys.argv[1] #decoded
-f2 = sys.argv[2] #reference
-#f1 = "result_data/input_ids_e0"
-#f2 = "result_data/response_ids_e0" 
+#f1 = sys.argv[1] #decoded
+#f2 = sys.argv[2] #reference
+#f3 = sys.argv[3] #highest index
+f1 = "result_data/input_ids_e"
+f2 = "result_data/response_ids_e" 
+f3 = 6
 ref = []
 decoded = []
 
-print("Reading data...")
-for i, j in zip(sorted(glob.glob(f1+'*.txt')),sorted(glob.glob(f2+'*.txt'))):
-        ref_tex = ''
-        dec_tex = ''
-        for k in open(i).readlines():
-                dec_tex = dec_tex + k.strip()
-        for l in open(j).readlines():
-                ref_tex = ref_tex + l.strip()
-        ref.append(ref_tex)
-        decoded.append(dec_tex)
+bl_scores = []
+r1_scores = []
+r2_scores = []
 
-# Blue
-print("\nCalculating blue score...")
-bl = bleu.moses_multi_bleu(decoded, ref)
-print(bl)
+for idx in range(f3+1):
+  idx = str(idx)
+  print("\n\n\nResults for index", idx)
+  for i, j in zip(sorted(glob.glob(f1+idx+'*.txt')),sorted(glob.glob(f2+idx+'*.txt'))):
+    ref_tex = ''
+    dec_tex = ''
+    for k in open(i).readlines():
+      dec_tex = dec_tex + k.strip()
+    for l in open(j).readlines():
+      ref_tex = ref_tex + l.strip()
+    ref.append(ref_tex)
+    decoded.append(dec_tex)
+  
+  # Blue
+  print("\nBlue score...")
+  bl = bleu.moses_multi_bleu(decoded, ref)
+  print(bl)
+  bl_scores.append(bl)
+  
+  # Rouge 1
+  print("\nRouge 1 score...")
+  f1_score, precision, recall = rouge.rouge_n(decoded, ref, 1)
+  print(f1_score*100)#, precision, recall)
+  r1_scores.append(f1_score*100)
+  
+  # Rouge 2
+  print("\nRouge 2 score...")
+  f1_score, precision, recall = rouge.rouge_n(decoded, ref, 2)
+  print(f1_score*100)#, precision, recall)
+  r2_scores.append(f1_score*100)
+  
+  ## Rouge l
+  #print("\nCalculating the rouge l score...")
+  #f1_score, precision, recall = rouge.rouge_l_sentence_level(decoded, ref)
+  #print(f1_score, precision, recall)
 
-# Rouge 1
-print("\nCalculating the rouge 1 score...")
-f1_score, precision, recall = rouge.rouge_n(decoded, ref, 1)
-print(f1_score, precision, recall)
 
-# Rouge 2
-print("\nCalculating the rouge 2 score...")
-f1_score, precision, recall = rouge.rouge_n(decoded, ref, 2)
-print(f1_score, precision, recall)
-
-## Rouge l
-#print("\nCalculating the rouge l score...")
-#f1_score, precision, recall = rouge.rouge_l_sentence_level(decoded, ref)
-#print(f1_score, precision, recall)
-
-
-
-
+x = list(range(1, f3+2))
+plt.plot(x, bl_scores, label="bl scores")
+plt.plot(x, r1_scores, label="r1 scores")
+plt.plot(x, r2_scores, label="r2 scores")
+plt.legend()
+plt.xlabel("Epoch")
+plt.ylabel("Score out of 100")
+plt.show()
 
 
 
